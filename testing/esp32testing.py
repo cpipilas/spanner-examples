be6c3beff01d06b150d960c902d0478195f4d817
cpipilas <30462769+cpipilas@users.noreply.github.com>
2019-02-25 15:13:14 +0200

    Update esp32testing.py

--- testing/esp32testing.py
+++ testing/esp32testing.py
@@ -4,19 +4,29 @@ from Testboard import Testboard
 
 testboard = Testboard("testboard_name")
 
-# Our Product's Input will be connected the Testboard's Pin D3, making it our 
-# Output Pin
-OUTPUT_PIN = "D3"
+# Our device's 1st Output Pin will be connected to the Testboard's D7, making it
+# our Input Pin 1
+INPUT_PIN_1 = "D7"
+# Our device's 2nd Output Pin will be connected to the Testboard's D5, making it
+# our Input Pin 2
+INPUT_PIN_2 = "D5"
 
-def toggle_digital_output():
-    # set PIN state
-    value=testboard.digitalWrite(OUTPUT_PIN, 'HIGH')
-    spanner.assertTrue(value)
-    
-    time.sleep(2)
+def validate_digital_input_high():
+    # check PIN state
+    value = testboard.digitalRead(INPUT_PIN_1)
 
-    value=testboard.digitalWrite(OUTPUT_PIN, 'LOW')
-    spanner.assertFalse(value)
+    Spanner.assertTrue(value);
+
+def validate_digital_input_low():
+    # check PIN state
+    value = testboard.digitalRead(INPUT_PIN_2)
+
+    Spanner.assertFalse(value);
 
 if __name__ == "__main__":
-    toggle_digital_output()
+
+    validate_digital_input_high()
+
+    time.sleep(2)
+
+    validate_digital_input_low()
