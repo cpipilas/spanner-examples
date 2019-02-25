728e83670025326a78d7f41d11e99c6d8746d844
cpipilas <30462769+cpipilas@users.noreply.github.com>
2019-02-25 15:15:22 +0200

    Update esp32testing.py

--- testing/esp32testing.py
+++ testing/esp32testing.py
@@ -6,27 +6,14 @@ testboard = Testboard("testboard_name")
 
 # Our device's 1st Output Pin will be connected to the Testboard's D7, making it
 # our Input Pin 1
-INPUT_PIN_1 = "D7"
-# Our device's 2nd Output Pin will be connected to the Testboard's D5, making it
-# our Input Pin 2
-INPUT_PIN_2 = "D5"
-
-def validate_digital_input_high():
-    # check PIN state
-    value = testboard.digitalRead(INPUT_PIN_1)
-
-    Spanner.assertTrue(value);
+INPUT_PIN_1 = "D3"
 
 def validate_digital_input_low():
     # check PIN state
-    value = testboard.digitalRead(INPUT_PIN_2)
+    value = testboard.digitalRead(INPUT_PIN_1)
 
     Spanner.assertFalse(value);
 
 if __name__ == "__main__":
-
-    validate_digital_input_high()
-
-    time.sleep(2)
 
     validate_digital_input_low()
