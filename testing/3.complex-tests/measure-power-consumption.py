0236a55f9a4dcc951c9309a0fc10e7ab0b744778
cpipilas <30462769+cpipilas@users.noreply.github.com>
2019-03-12 15:33:16 +0200

    Update measure-power-consumption.py

--- testing/3.complex-tests/measure-power-consumption.py
+++ testing/3.complex-tests/measure-power-consumption.py
@@ -29,29 +29,29 @@ testboard = Testboard("testboard_name")
 # Our Testboard's D3 Pin is connected to a power switching circuit that controls
 # the power going to the device. When we toggle it HIGH, the device will be
 # powered, when LOW, the device will shut down
-OUTPUT_PIN = "D3"
+#OUTPUT_PIN = "D3"
 
 def measure_power_consumption():
 
     # Turn the device off
-    testboard.digitalWrite(OUTPUT_PIN, "LOW");
+    #testboard.digitalWrite(OUTPUT_PIN, "LOW");
 
     # Wait for a while for it to shut down
-    time.sleep(10)
+    #time.sleep(10)
 
     # Turn the device back on
-    testboard.digitalWrite(OUTPUT_PIN, "HIGH");
+    #testboard.digitalWrite(OUTPUT_PIN, "HIGH");
 
     # The device runs some initialization actions in the beginning, which are
     # not indicative of the true power consumption. Therefore we wait for a
     # while for the initial conditions to pass
-    time.sleep(90)
+    time.sleep(5)
 
     # Start measuring power consumption
     testboard.startPowerMeasurement()
 
     # Measure for 5 minutes
-    time.sleep(5*60)
+    time.sleep(30)
 
     # Stop measuring power consumption
     testboard.stopPowerMeasurement()
