58ba8226083cfced82c92be67a31b8b35f55adac
cpipilas <30462769+cpipilas@users.noreply.github.com>
2019-05-02 11:53:29 +0300

    Update measure-power-consumption.py

--- testing/3.complex-tests/measure-power-consumption.py
+++ testing/3.complex-tests/measure-power-consumption.py
@@ -34,11 +34,11 @@ def test_measure_power_consumption():
 
     # Get an instant measurement
     i = 1
-    while i < 500: 
+    while i < 100: 
         current_ma = testboard.ina219_getValue(INA219.CURRENT_MA)
         print (current_ma)
         assert (current_ma < 30 and current_ma > 29.5)
-        time.sleep(0.001)
+        time.sleep(0.1)
         i += 1
     
     # Make sure the total power consumption didn't exceed 100mAh
