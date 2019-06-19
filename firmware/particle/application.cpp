9c21d1d59141952a642b0f1537ec39ea6fa10664
cpipilas <30462769+cpipilas@users.noreply.github.com>
2019-06-19 14:13:39 +0300

    Update application.cpp

--- firmware/particle/application.cpp
+++ firmware/particle/application.cpp
@@ -47,9 +47,6 @@ It blinks the D7 LED on your Particle device. If you have an LED wired to D0, it
 // First, we're going to make some variables.
 // This is our "shorthand" that we'll use throughout the program:
 
-//int led1 = D1; // Instead of writing D0 over and over again, we'll write led1
-// You'll need to wire an LED to this one to see it blink.
-
 int led2 = D7; // Instead of writing D7 over and over again, we'll write led2
 // This one is the little blue LED on your board. On the Photon it is next to D7, and on the Core it is next to the USB jack.
 
@@ -64,7 +61,6 @@ void setup() {
 
   // It's important you do this here, inside the setup() function rather than outside it or in the loop function.
 
-  //pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
 
 }
@@ -75,15 +71,13 @@ void setup() {
 
 void loop() {
   // To blink the LED, first we'll turn it on...
-  //digitalWrite(led1, HIGH);
   digitalWrite(led2, HIGH);
 
   // We'll leave it on for 1 second...
-  delay(1000);
+  //delay(1000);
 
   // Then we'll turn it off...
-  //digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
+  //digitalWrite(led2, LOW);
 
   // Wait 1 second...
   delay(1000);
